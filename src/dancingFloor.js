221278f2b168324393f62a6f94d0e55baf35e9ab
Giovanny Beltran <ekeisco@gmail.com>
2018-04-21 13:11:05 -0500

    fix effect location bug

--- src/dancingFloor.js
+++ src/dancingFloor.js
@@ -12,13 +12,21 @@ export default class DancingFloor {
     this.cellWidth = 100
     this.cellHeight = 100
 
+    // cell matrix
+    this.cells = []
+
+
     // setup a dancing floor of 6x6 cells with a fixed size
     let offsetX = this.x + this.cellWidth/2
     let offsetY = this.y + this.cellHeight/2
 
     for (let j = 0; j < this.rows; j++) {
+      this.cells.push([])
       for (let i = 0; i < this.cols; i++) {
-        this.add.sprite(i*this.cellWidth + offsetX, j*this.cellHeight + offsetY, 'cell')
+        let cell = new Cell({
+          sprite: this.add.sprite(i*this.cellWidth + offsetX, j*this.cellHeight + offsetY, 'cell')
+        })
+        this.cells[j].push(cell)
       }
     }
   }
@@ -27,5 +35,45 @@ export default class DancingFloor {
     let minionOffsetX = this.x + 40
     let minionOffsetY = this.y + 40
     this.add.sprite(i*this.cellWidth + minionOffsetX, j*this.cellHeight + minionOffsetY, 'minion')
+
+    let affectEffect = [
+      [0,1,0],
+      [1,0,1],
+      [0,1,0]
+    ]
+
+    this.cells[j][i].addMinion()
+
+    let radiusX = (affectEffect[0].length - 1) / 2
+    let radiusY = (affectEffect.length - 1) / 2
+
+    let lowerLimitX = ((i-radiusX) < 0) ? 0 : i-radiusX
+    let lowerLimitY = ((j-radiusY) < 0) ? 0 : j-radiusY
+    let upperLimitX = (i + radiusX >= this.cols) ? this.cols - 1 : i + radiusX
+    let upperLimitY = (j + radiusY >= this.rows) ? this.rows - 1 : j + radiusY
+
+    for (var lj = lowerLimitY; lj <= upperLimitY; lj++) {
+      let localRow = affectEffect[lj - (j - radiusY)]
+      for (var li = lowerLimitX; li <= upperLimitX; li++) {
+        let value = localRow[li - (i - radiusX)]
+        if(value!=0) {
+          this.cells[lj][li].affects()
+        }
+      }
+    }
+  }
+}
+
+// helper class
+class Cell {
+  constructor (config) {
+    this.sprite = config.sprite
+  }
+
+  addMinion (minion) {
+    this.sprite.tint = 0xff0000
+  }
+  affects () {
+    this.sprite.tint = 0x00ff00
   }
 }
